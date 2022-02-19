9e515f9339a8ea40b6371a83afcdea9828d1f2c1
sugan0tech <sugankpms@gmail.com>
2022-02-19 06:53:54 +0530

    added user schema with email

--- node/models/user.js
+++ node/models/user.js
@@ -1,9 +1,21 @@
 const mongoose = require("mongoose");
+const validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
 const userSchema = mongoose.Schema({
     name: {
         type: String,
         required: true,
         lowercase: true
+    },
+    mail: {
+        type: String,
+        required: true,
+        lowercase: true,
+        validate: [validateEmail, "pease fill valid mail"],
+        trim: true, // trims white spaces from right and left
+        unique: true
     },
     password: {
         type: String,
