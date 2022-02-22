5a490a56094fced683d4b983e03272d7a2322b8e
hari-krishna-tech <jvenkadam@gmail.com>
2022-02-22 21:49:35 +0530

    nothing

--- node/db.js
+++ node/db.js
@@ -1,11 +1,17 @@
 const mongoose = require("mongoose");
-require('dotenv').config()
+require("dotenv").config();
 const url = process.env.DB_URL;
 const chalk = require("chalk");
-mongoose.connect(url, () => {
+mongoose.connect(
+  url,
+  () => {
     console.log(chalk.green.bold.inverse("Connected to the database"));
-}, (err) => {
-    console.log(chalk.red.bold.inverse("Not Connected to Database err!! occurred"));
-});
+  },
+  (err) => {
+    console.log(
+      chalk.red.bold.inverse("Not Connected to Database err!! occurred")
+    );
+  }
+);
 
 module.exports = mongoose;
