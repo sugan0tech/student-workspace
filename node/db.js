38c814deefbfb6346d8e5128fa90e30ba653be2c
sugan0tech <sugankpms@gmail.com>
2022-02-22 14:32:34 +0530

    ignore

--- node/db.js
+++ node/db.js
@@ -5,7 +5,7 @@ const chalk = require("chalk");
 mongoose.connect(url, () => {
     console.log(chalk.green.bold.inverse("Connected to the database"));
 }, (err) => {
-    console.log(chalk.red.bold.inverse("Not Connected to Database err!! occured"));
+    console.log(chalk.red.bold.inverse("Not Connected to Database err!! occurred"));
 });
 
 module.exports = mongoose;
