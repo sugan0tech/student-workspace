97897d86d8800f648a2961c248cdfa9efabc9c96
hari-krishna-tech <jvenkadam@gmail.com>
2022-02-26 08:23:28 +0530

    checkS

--- node/apis/get-assignments.js
+++ node/apis/get-assignments.js
@@ -19,82 +19,57 @@ const tok = require("../functions/token");
     },]
  */
 route
-    .use(express.json())
-    .use(express.urlencoded({ extended: true }))
-    .use(cookieParser());
+  .use(express.json())
+  .use(express.urlencoded({ extended: true }))
+  .use(cookieParser());
 
 route.route("/").post((req, res) => {
-<<<<<<< HEAD
   console.log(
     chalk.yellow.bold.inverse("\n    get-assignment post request    \n")
   );
   console.log(chalk.green("request api : "), req.body);
   console.log(chalk.green("request cookie :"), req.cookies);
-  const verifiedStatus = tok.verify(req.cookies.token, req.cookies.email);
-  console.log(
-    chalk.bold.green.inverse("token verified status :"),
-    verifiedStatus
-  );
-  if (verifiedStatus) {
-    const data = tok.getPayload(req.cookies.token);
-    func.getinfo(data.email, data.password).then(
-      (resolve) => {
-        if (resolve != null) {
-          console.log(chalk.green.bold("\n\tuser info found\n"));
-          console.log(resolve.assignments);
-          res.send(resolve.assignments);
-        } else {
-          res.send("user infor can't be fetched");
-        }
-      },
-      (e) => {
-        console.log(
-          chalk.red.bold("\n\terror in get-assignments\n"),
-          chalk.red.bold.inverse("\tlocation: ./api/get-assignments\n")
-        );
-      }
-=======
-    console.log(
-        chalk.yellow.bold.inverse("\n    get-assignment post request    \n")
->>>>>>> d0d8244324a2924843d2e8ef70978f3f8830bc2e
-    );
-    console.log(chalk.green("request api : "), req.body);
-    console.log(chalk.green("request cookie :"), req.cookies);
-    if (req.cookies.token != null) {
-        /*returns js object 
+  if (req.cookies.token != null) {
+    /*returns js object 
         {
             valid: bool,
             payload: object
         } */
-        const tokenVerificationData = tok.verify(req.cookies.token, req.cookies.email);
-        console.log(
-            chalk.bold.green.inverse("token verified status :"),
-            tokenVerificationData.valid
+    const tokenVerificationData = tok.verify(
+      req.cookies.token,
+      req.cookies.email
+    );
+    console.log(
+      chalk.bold.green.inverse("token verified status :"),
+      tokenVerificationData.valid
+    );
+    console.log(tokenVerificationData);
+    if (tokenVerificationData.valid) {
+      func
+        .getinfo(
+          tokenVerificationData.payload.email,
+          tokenVerificationData.payload.password
+        )
+        .then(
+          (resolve) => {
+            func.getAssignments(resolve.assignments).then(
+              (resolve) => {
+                res.send(resolve);
+              },
+              (e) => {
+                console.log(e);
+              }
+            );
+          },
+          (e) => {
+            console.log(e);
+          }
         );
-        console.log(tokenVerificationData)
-        if (tokenVerificationData.valid) {
-            func.getinfo(tokenVerificationData.payload.email, tokenVerificationData.payload.password).then(
-                (resolve) => {
-                    func.getAssignments(resolve.assignments).then(
-                        (resolve) => {
-                            res.send(resolve);
-                        },
-                        (e) => {
-                            console.log(e)
-                        }
-
-                    );
-
-                },
-                (e) => {
-                    console.log(e);
-                }
-            )
-        } else {
-            res.clearCookie("token");
-            res.redirect(301, "/login");
-        }
+    } else {
+      res.clearCookie("token");
+      res.redirect(301, "/login");
     }
+  }
 });
 
 module.exports = route;
